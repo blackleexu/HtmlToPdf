92071ee6dd8a88c79f15c28f86d2b8e80fd3ef87
blackleexu <446634431@qq.com>
2020-03-03 15:33:33 +0800

    table\pagebreak

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 
-include('./mpdf/Mpdf.php');
+include('./mpdf/mpdf.php');
 
 function htmlToPdf(){
     //这里需要指定zh-cn格式 要不然中文会乱码
     $mpdf=new mpdf('zh-cn','A4',0,'',30,30);
     $mpdf->SetDisplayMode('real');
 
-    $html=file_get_contents("./html2pdf_template.html");
+    $html=file_get_contents("./normal_table.html");
     $mpdf->autoScriptToLang = true;
     $mpdf->autoLangToFont = true;
     $mpdf->WriteHTML($html);
-//    $mpdf->WriteHTML('<h1>hello world</h1>');
     //在指定位置添加一张图片（有的开发者用这个来做签章，但是这个位置并不好控制）
 //    $mpdf->Image('./attachment.jpg',100,100);
 
@@ -19,5 +18,23 @@ function htmlToPdf(){
 }
 
 htmlToPdf();
-
 echo 'success';
+
+function htmlStrToPdf(){
+    //这里需要指定zh-cn格式 要不然中文会乱码
+    $mpdf=new mpdf('zh-cn','A4',0,'',30,30);
+    $mpdf->SetDisplayMode('real');
+
+    $html='<p style="text-align: center;color: #FFAAAA">hello world!</p>';
+    $mpdf->autoScriptToLang = true;
+    $mpdf->autoLangToFont = true;
+    $mpdf->WriteHTML($html);
+    //在指定位置添加一张图片（有的开发者用这个来做签章，但是这个位置并不好控制）
+//    $mpdf->Image('./attachment.jpg',100,100);
+
+    $mpdf->Output('tem'.time().'.pdf','F');//保存成pdf文件
+}
+
+
+//htmlStrToPdf();
+//echo 'success';
